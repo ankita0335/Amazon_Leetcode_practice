b57cfcfc26a88acc02ccf154a09b0eaadb43e2b2
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2024-09-20 23:02:54 +0530

    Time: 10 ms (32.38%), Space: 11.8 MB (38.92%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void computeLPS(string pattern, vector<int>& lps) {
+        int M = pattern.length();
+        int len = 0;
+        lps[0] = 0;
+        int i = 1;
+        while (i < M) 
+        {
+            if (pattern[i] == pattern[len]) 
+            {
+                len++;
+                lps[i] = len;
+                i++;
+            } 
+            else 
+            {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+    string shortestPalindrome(string s) 
+    {
+        string rev = s;
+        reverse(begin(rev), end(rev));
+        string temp = s + "-" + rev;
+        vector<int> LPS(temp.length(), 0); //O(n)
+        computeLPS(temp, LPS);
+        int longestLPSLength = LPS[temp.length()-1]; //temp.back();
+        string culprit = rev.substr(0, s.length() - longestLPSLength); //O(n)
+        return culprit + s;
+    }
+};
