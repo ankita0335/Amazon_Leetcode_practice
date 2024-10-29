8a4cca43fce5e226e418d4a29f5268db98824b12
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2024-10-29 15:56:21 +0530

    Time: 53 ms (24.2%), Space: 60.3 MB (27.2%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -1,25 +1,40 @@
 class Solution {
-private:
-    string invert(string s){
-        string temp;
-        for(int i=0; i<s.size(); i++){
-            if(s[i] == '0') temp.push_back('1');
-            else temp.push_back('0');
-        }
-        return temp;
-    }
 public:
-    char findKthBit(int n, int k) {
-        if(k == 1) return '0';
-        vector<string> v;
-        string s = "0";
-        v.push_back(s);
-        for(int i=1; i<n; i++){
-            string x = invert(v[i-1]);
-            reverse(x.begin(), x.end());
-            string str = v[i-1]+"1"+ x;
-            v.push_back(str);
+    string invertkro(string s)
+    {
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='0')
+            {
+                s[i]='1';
+            }
+            else
+                s[i]='0';
         }
-        return v[n-1][k-1];
+        return s;
+    }
+    string reverse(string s)
+    {
+        int n = s.length(); 
+        for (int i=0; i<n/2; i++) 
+        {
+            swap(s[i], s[n-1-i]); 
+        }
+        return s; 
+    }
+    char findKthBit(int n, int k) 
+    {
+        string s = "0";
+        for(int i=1;i<n;i++)
+        {
+            s += '1'+reverse(invertkro(s));
+        }
+        //cout<<s<<endl;
+        for(int i=0;i<k;i++)
+        {
+            if(i==k-1)    
+                return s[i];
+        }
+        return {};
     }
 };
