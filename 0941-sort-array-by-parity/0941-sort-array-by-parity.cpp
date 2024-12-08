de50906cda2a3bebfe416461e8ec7dff60b5dc84
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2024-12-09 00:10:34 +0530

    Time: 1 ms (20.44%), Space: 20.4 MB (15.77%) - LeetHub

---
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        partition(nums.begin(),nums.end(),[](int n){return n%2==0;});
+            return nums;
+    }
+};
