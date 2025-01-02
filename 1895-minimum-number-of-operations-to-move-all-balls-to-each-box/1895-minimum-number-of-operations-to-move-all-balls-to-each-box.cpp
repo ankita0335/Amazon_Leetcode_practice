8dbe171e0cef5e5556f50da9d4fdad72a2200c64
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2025-01-02 16:31:08 +0530

    Time: 6 ms (56.12%), Space: 12.9 MB (5.95%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     vector<int> minOperations(string boxes) 
     {
-        vector<int>positionOf1s;
-        for(int i=0; i< boxes.size(); i++)
+        int n= boxes.size();
+        vector<int>prefixarr(n,0);
+        vector<int>suffixarr(n,0);
+        int sum = boxes[0] - '0';
+        for(int i = 1; i < n; i++)
         {
-            if(boxes[i] == '1')
-            {
-                positionOf1s.push_back(i);
-            }
+            prefixarr[i] = prefixarr[i-1]+sum;
+            sum += boxes[i] - '0';
+        }
+        sum = boxes[n-1]-'0';
+        for(int i = n-2; i >= 0 ; i--)
+        {
+            suffixarr[i] = suffixarr[i+1]+sum;
+            sum += boxes[i]-'0';
         }
         vector<int>ans;
-        int j=0;
-        while(j < boxes.size())
+        for(int i = 0; i < prefixarr.size(); i++)
         {
-            int sum=0;
-            for(int i=0;i<positionOf1s.size(); i++)
-            {
-                sum+=abs(positionOf1s[i]-j);
-            }
-            ans.push_back(sum);
-            j++;
+            ans.push_back(prefixarr[i]+suffixarr[i]);
         }
         return ans;
     }
