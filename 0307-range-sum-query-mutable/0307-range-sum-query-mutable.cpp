5c8e4461bbe695659d3ab76280a1d495709b812c
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2025-02-19 00:07:47 +0530

    Time: 63 ms (57.52%), Space: 182.3 MB (48.56%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -1,41 +1,60 @@
 class NumArray {
 public:
-    vector<int>arr;
-    vector<int>pre;
-    int len;
-    NumArray(vector<int>& nums) {
-        int n=nums.size();
-        len=sqrt(n)+1;
-        pre.resize(len);
-        arr= nums;
-        for(int i=0;i<n;i++)
+    int n ;
+    vector<int>segment;
+    void updateq(int idx, int val, int i, int l, int r)
+    {
+        if(l == r)
         {
-            pre[i/len] +=nums[i];
+            segment[i] = val;
+            return ;
         }
+        int mid = (l+r)/2;
+        if(idx <= mid)
+            updateq(idx, val, 2*i+1, l, mid);
+        else 
+            updateq(idx, val , 2*i+2, mid+1, r);
+        segment[i] = segment[2*i+1]+ segment[2*i+2];
+    }
+
+    int find( int idx, int l , int r, int left, int right)
+    {
+        if(l>= left && r<= right)
+        {
+            return segment[idx];
+        }
+        if(left> r || right < l )   return 0;
+
+        int mid = (l+r)/2;
+        int lvalue = find( 2*idx+1, l , mid, left, right);
+        int rvalue = find(2*idx+2, mid+1, r, left , right);
+        return lvalue + rvalue;
+    }
+    void create( vector<int>&nums, int idx, int l , int r)
+    {
+        if(l==r)
+        {
+            segment[idx] = nums[l];
+            return ;
+        }
+        int mid = (l+r)/2;
+        create(nums, 2*idx+1, l , mid);
+        create(nums, 2*idx+2, mid+1, r);
+        segment[idx]= segment[2*idx+1] + segment[2*idx+2];
+    }
+    NumArray(vector<int>& nums) 
+    {
+        n= nums.size();
+        segment.resize(4*n);
+        create(nums,0, 0, n-1 );
     }
     
     void update(int index, int val) {
-        int block = index / len; 
-        pre[block] -= arr[index];
-        pre[block] += val; 
-        arr[index]= val; 
+        updateq(index, val, 0 , 0 , n-1);
     }
+    
     int sumRange(int left, int right) {
-    int sum=0;
-        for(int i=left;i<=right;)
-        {
-            if(i%len==0 && i+len-1<=right)
-            {
-                sum+=pre[i/len];
-                i+=len;
-            }
-            else
-            {
-                sum+=arr[i];
-                i++;
-            }
-        }
-        return sum;
+        return find ( 0, 0 , n-1, left, right);
     }
 };
 
