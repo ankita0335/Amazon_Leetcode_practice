b51c2f0b8051c1396359dabe30e3a29c68114411
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2025-08-02 11:33:50 +0530

    Time: 2 ms (54.94%), Space: 7.9 MB (68.7%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,7 +4,20 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        while(isBadVersion(n))   n--;
-        return n+1;
+        int left = 1;
+        int r = n;
+        int ans = -1;
+        while(left<=r)
+        {
+            int mid= left+ (r- left)/2;
+            if(isBadVersion(mid))
+            {
+                ans = mid;
+                r = mid-1;
+            } 
+            else
+                left = mid+1;
+        }
+        return ans;
     }
 };
