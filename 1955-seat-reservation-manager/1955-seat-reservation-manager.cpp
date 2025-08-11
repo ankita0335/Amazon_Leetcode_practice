7dca26335eb29cbb4b48f6bfb4e73ddfb9581cce
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2025-08-11 09:16:02 +0530

    Time: 58 ms (88.83%), Space: 149.5 MB (95.11%) - LeetHub

--- 1955-seat-reservation-manager/1955-seat-reservation-manager.cpp
+++ 1955-seat-reservation-manager/1955-seat-reservation-manager.cpp
@@ -1,16 +1,20 @@
 class SeatManager {
 public:
-    priority_queue<int, vector<int> ,greater<int>>pq;
+    priority_queue<int, vector<int>, greater<int>>pq;
+    int seatmark =1;
     SeatManager(int n) {
-        for(int i=1;i<=n;i++)
-        {
-            pq.push(i); //O(log(n))
-        }
+        
     }
     
     int reserve() {
-        int seat = pq.top(); //O(1)
-        pq.pop();
+        if(!pq.empty())
+        {
+            int seat = pq.top();
+            pq.pop();
+            return seat;
+        }
+        int seat = seatmark;
+        seatmark++;
         return seat;
     }
     
@@ -18,7 +22,6 @@ public:
         pq.push(seatNumber);
     }
 };
-
 
 /* brute force: 
 class SeatManager {
@@ -44,6 +47,30 @@ public:
         seat[seatNumber] =-1;
     }
 };  */
+ 
+ /* Optimal:
+ 
+class SeatManager {
+public:
+    priority_queue<int, vector<int> ,greater<int>>pq;
+    SeatManager(int n) {
+        for(int i=1;i<=n;i++)
+        {
+            pq.push(i); //O(log(n))
+        }
+    }
+    
+    int reserve() {
+        int seat = pq.top(); //O(1)
+        pq.pop();
+        return seat;
+    }
+    
+    void unreserve(int seatNumber) {
+        pq.push(seatNumber);
+    }
+};
+*/
 /**
  * Your SeatManager object will be instantiated and called as such:
  * SeatManager* obj = new SeatManager(n);
