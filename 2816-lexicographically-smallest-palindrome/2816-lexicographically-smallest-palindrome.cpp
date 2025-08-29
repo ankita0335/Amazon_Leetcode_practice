a41575903a156356c790fe45565fd7ce87339a1a
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2025-08-29 10:03:39 +0530

    Time: 9 ms (21.3%), Space: 18.5 MB (86.54%) - LeetHub

---
+++ 2816-lexicographically-smallest-palindrome/2816-lexicographically-smallest-palindrome.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int n = s.size();
+        int i=0, j=n-1;
+        while(i<n)
+        {
+            if(s[i] != s[j])
+            {
+                if(s[i]<s[j]) 
+                    s[j]=s[i];
+                else
+                    s[i]= s[j];
+            }
+            j--;i++;
+        }
+        return s;
+    }
+};
