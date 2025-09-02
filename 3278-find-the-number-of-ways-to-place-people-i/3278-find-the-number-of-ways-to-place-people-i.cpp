a352c50659d27bc59a3090ac0656db3a8d1feafb
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2025-09-02 16:48:49 +0530

    Time: 7 ms (66.67%), Space: 32.7 MB (22.22%) - LeetHub

--- 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
+    static bool comp(const vector<int>& a, const vector<int>& b) {
+        if (a[0] != b[0]) return a[0] < b[0];
+        return a[1] > b[1];
+    }
     int numberOfPairs(vector<vector<int>>& points) {
         int n = points.size();
         int ans = 0;
+        sort(points.begin(), points.end(),comp);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (i==j)   continue;
                 if (points[i][0] <= points[j][0] && points[i][1] >= points[j][1]){
                     bool valid = true;
-                    for (int k = 0; k < n; k++) {
-                        if (k == i || k == j) continue;
+                    for (int k = i+1; k < j; k++) {
                         int kx = points[k][0];
                         int ky = points[k][1];
                         if (kx >= points[i][0] && kx <= points[j][0] &&
