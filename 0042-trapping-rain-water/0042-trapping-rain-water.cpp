deca370376120d547c3b1e07a23a7070291f407b
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2026-04-09 10:47:13 +0530

    Time: 0 ms (100%), Space: 26 MB (56.08%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,40 +2,44 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        int lmx=0;
-        int rmx=0;
-        int idx=0;
-        int ans=0;
-        int lmaxi= height[0];
-        for(int i=0;i<n;i++)
-        {
-            if(height[i]> lmaxi)
-            {
-                lmaxi= height[i];
-                idx = i;
+        int baridx = 0;
+        int maxbar = height[0];
+        int ans = 0;
+        
+        for(int i=0; i<n; i++){
+            if(height[i] > maxbar){
+                maxbar = height[i];
+                baridx = i;
             }
         }
-        for(int i=0;i<idx;i++)
-        {
-            if(lmx> height[i])
-            {
-                ans+= lmx- height[i];
+
+        int lmax =0 , rmax = 0;
+
+        for(int i = 0 ; i< baridx; i++){
+            if(lmax > height[i]){
+                ans += lmax - height[i];
             }
             else
-            {
-                lmx= height[i];
-            }
+                lmax = height[i];
         }
-        // for right side
-        for(int i=n-1;i>idx;i--)
-        {
-            if(rmx > height[i])
-            {
-                ans+= rmx- height[i];
+
+        for(int i=n-1; i> baridx; i--){
+            if(rmax > height[i]){
+                ans += rmax - height[i];
             }
-            else 
-                rmx= height[i];
+            else
+                rmax = height[i];
         }
         return ans;
     }
 };
+
+/*I first find the index of the maximum height bar. This splits the array into two parts.
+
+Then I traverse from left to max index, maintaining left max and calculating trapped water.
+Similarly, I traverse from right to max index, maintaining right max.
+
+This works because the highest bar acts as a boundary, so each side can be processed independently.
+
+The solution runs in O(n) time and O(1) space.
+*/
